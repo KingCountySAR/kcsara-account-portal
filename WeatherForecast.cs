5759455b79b0156cc86293e02d0c24da6e1019c3
Matt Cosand <matt@cosand.net>
2020-03-22 22:53:16 -0700

    boilerplate

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace kcsara_account_portal
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
